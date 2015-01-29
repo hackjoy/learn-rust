6a70d26ed37c5bb10b67ffb24b80d2cfcf709918
Jack Hoy <jackhoy05@gmail.com>
2015-01-29 19:29:57 +0000

    move old code

--- hello.rs
+++ hello.rs
@@ -1,3 +1,12 @@
 fn main() {
-  println!("Hello, world!");
+    println!("Hello, world!");
 }
+
+// main is a special function that denotes the start of a rust program
+// println! is a macro rather than a function
+// the string is statically allocated
+// ; is the end of an expression
+
+// rust is a compiled language
+// rustc hello.rs gives us a binary executable
+// ./hello executes it
---
+++ pre-1.0/hello_single.rs
@@ -0,0 +1,6 @@
+fn main() {
+  for num in range(0, 10) {
+    let greeting_message = "Hello";
+    println!("{} I am process number {}", greeting_message, num);
+  }
+}
