832c43acc50600ec8ec02648e0afe5f192f2e335
Jack Hoy <jackhoy05@gmail.com>
2023-02-15 19:58:43 +0000

    add spec for ok list

--- lists/src/ok_list.rs
+++ lists/src/ok_list.rs
@@ -41,3 +41,30 @@ impl Drop for List {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn basics() {
+        use super::List;
+
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
