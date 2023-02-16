9e52175a48452cc99e9728e78c8821b23ced8d7e
Jack Hoy <jackhoy05@gmail.com>
2023-02-16 11:42:09 +0000

    implement IntoIter for List

--- lists/src/ok_list.rs
+++ lists/src/ok_list.rs
@@ -11,9 +11,16 @@ struct Node<T> {
     next: Link<T>,
 }
 
+// Tuple struct - useful as a wrapper type
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
     pub fn push(&mut self, elem: T) {
@@ -41,6 +48,14 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop() // access first item of tuple
+    }
+}
+
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
@@ -55,7 +70,6 @@ mod test {
     #[test]
     fn basics() {
         use super::List;
-
         let mut list = List::new();
 
         assert_eq!(list.pop(), None);
@@ -94,5 +108,20 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        use super::List;
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
