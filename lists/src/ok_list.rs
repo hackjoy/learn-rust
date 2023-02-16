72f1c0a96a4bae0abf236e144b75104768d84d48
Jack Hoy <jackhoy05@gmail.com>
2023-02-16 11:26:09 +0000

    implement peek and peek_mut

--- lists/src/ok_list.rs
+++ lists/src/ok_list.rs
@@ -25,6 +25,14 @@ impl<T> List<T> {
         self.head = Some(new_node);
     }
 
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+
     pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
@@ -36,7 +44,7 @@ impl<T> List<T> {
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
-        while let Some(mut boxed_node) = cur_link {
+        while let Some(boxed_node) = cur_link {
             cur_link = self.head.take();
         }
     }
@@ -66,5 +74,25 @@ mod test {
         assert_eq!(list.pop(), Some(4));
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        use super::List;
+
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
